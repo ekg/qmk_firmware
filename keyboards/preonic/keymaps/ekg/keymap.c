4d2a8b4498850ec190c2e02b750511a6f9f9f06b
Erik Garrison <erik.garrison@gmail.com>
2025-09-26 10:23:41 -0500

    Add F-keys to LOWER layer and fix layer switching
    - F1-F11 on LOWER + top row (grave through 0)
    - F12 on LOWER + bottom right
    - Fix LOWER/RAISE layer switching (was incorrectly using MO(3))
    - LOWER+RAISE now properly activates ADJUST layer
    - Keep F-keys also available on ADJUST layer

--- keyboards/preonic/keymaps/ekg/keymap.c
+++ keyboards/preonic/keymaps/ekg/keymap.c
@@ -114,11 +114,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL,
-  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
-  BACKLIT, KC_ESC,  KC_LGUI, KC_LALT, _______, KC_SPC,  KC_SPC,  MO(3),   KC_BSPC, KC_UP,   KC_DOWN, KC_LEFT
+  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_F12,
+  BACKLIT, KC_ESC,  KC_LGUI, KC_LALT, _______, KC_SPC,  KC_SPC,  RAISE,   KC_BSPC, KC_UP,   KC_DOWN, KC_LEFT
 ),
 
 /* Raise
@@ -135,11 +135,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_TILD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
   KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
-  BACKLIT, KC_ESC,  KC_LGUI, KC_LALT, MO(3),   KC_SPC,  KC_SPC,  _______, KC_BSPC, KC_UP,   KC_DOWN, KC_LEFT
+  BACKLIT, KC_ESC,  KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  _______, KC_BSPC, KC_UP,   KC_DOWN, KC_LEFT
 ),
 
 /* Adjust (Lower + Raise)
